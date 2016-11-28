a5f1aaa079a7a59abbcdd4730fe3cacda6d866ee
Anderson Correa <email>
2016-11-28 04:20:36 -0200

    update

---
+++ app_controle_compras/JS/Config.js
@@ -0,0 +1,9 @@
+function setConfig() {
+    var text = {
+        "title": "Controle de Compras"
+    }
+    document.title = text.title;
+    document.getElementById("navTitle").innerHTML = text.title;
+}
+
+setConfig();
